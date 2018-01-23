7d8d841add60c0453af045e3152f32078ba257f1
Andrew Aitken-Fincham <andrew@silverstripe.com>
2018-01-23 17:14:07 +0000

    restrict images to logged in users

--- mysite/code/model/GalleryImageExtension.php
+++ mysite/code/model/GalleryImageExtension.php
@@ -3,6 +3,7 @@
 namespace AndrewAndante\Locket\Model;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Security\InheritedPermissions;
 
 class GalleryImageExtension extends DataExtension
 {
@@ -13,6 +14,11 @@ class GalleryImageExtension extends DataExtension
 //    private static $many_many = [
 //        'GalleryImageTags' => GalleryImageTag::class,
 //    ];
+
+    private static $defaults = [
+        'CanViewType' => InheritedPermissions::LOGGED_IN_USERS,
+        'CanEditType' => InheritedPermissions::LOGGED_IN_USERS,
+    ];
 
     public function getDisplayDate()
     {
