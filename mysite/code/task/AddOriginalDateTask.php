0dae54d9c78e4b68397642a4599b3b181a1fd76a
Andrew Aitken-Fincham <andrew@silverstripe.com>
2018-07-22 22:40:52 +0100

    add original date updater task

---
+++ mysite/code/task/AddOriginalDateTask.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AndrewAndante\Locket\Task;
+
+use DateTime;
+use SilverStripe\Assets\Image;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Environment;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\FieldType\DBDatetime;
+
+class AddOriginalDateTask extends BuildTask
+{
+
+    /**
+     * Implement this method in the task subclass to
+     * execute via the TaskRunner
+     *
+     * @param HTTPRequest $request
+     * @return
+     */
+    public function run($request)
+    {
+        $dryRun = $request->getVar('dryrun') ? true : false;
+        $count = 0;
+        $errorCount = 0;
+        foreach (Image::get() as $image) {
+            if (!$image->OriginalDate) {
+                $filePath = implode(['/', Environment::getEnv('ASSETS_PATH'), '.protected', $image->File->getMetaData()['path']]);
+                $exifData = exif_read_data($filePath);
+                if ($exifData && isset($exifData['DateTimeOriginal'])) {
+                    $dateTimeOriginal = DateTime::createFromFormat('Y:m:d H:i:s', $exifData['DateTimeOriginal']);
+                    $image->OriginalDate = DBDatetime::create()->setValue($dateTimeOriginal->format(DATE_ISO8601));
+                    if (!$dryRun) $image->write();
+                    ++$count;
+                } else {
+                    ++$errorCount;
+                }
+            }
+        }
+
+        echo Image::get()->count() . "total images<br>";
+        echo "$count images updated<br>";
+        echo "$errorCount images failed to update<br>";
+    }
+}
